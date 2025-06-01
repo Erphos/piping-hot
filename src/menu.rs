b3a1b362853dc8da98e18667de126fb3be2ca033
Ada Hieta <ada@hieta.fi>
2025-06-01 11:24:54 +0300

    feat: functional menu buttons

--- src/menu.rs
+++ src/menu.rs
@@ -9,7 +9,10 @@ pub struct MenuPlugin;
 impl Plugin for MenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(AppState::MainMenu), setup_main_menu)
-            .add_systems(Update, handle_buttons.run_if(in_state(AppState::MainMenu)))
+            .add_systems(
+                Update,
+                (update_button_color, menu_action).run_if(in_state(AppState::MainMenu)),
+            )
             .add_systems(OnExit(AppState::MainMenu), teardown_main_menu);
     }
 }
@@ -18,60 +21,99 @@ impl Plugin for MenuPlugin {
 #[derive(Component, Debug)]
 struct MenuItem;
 
+#[derive(Component, Debug)]
+enum MenuAction {
+    StartGame,
+    Quit,
+}
+
 fn setup_main_menu(mut commands: Commands, assets: Res<UiAssets>) {
-    commands.spawn(button("New Game", &assets));
+    commands
+        .spawn((
+            Node {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            MenuItem,
+        ))
+        .with_children(|cmd| {
+            cmd.spawn(button("New Game", &assets))
+                .insert(MenuAction::StartGame);
+            cmd.spawn(button("Quit", &assets)).insert(MenuAction::Quit);
+        });
 }
 
 fn button(text: &str, assets: &UiAssets) -> impl Bundle + use<> {
     (
+        Button,
         Node {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            align_items: AlignItems::Center,
+            width: Val::Px(250.0),
+            height: Val::Px(65.0),
+            border: UiRect::all(Val::Px(5.0)),
+            // horizontally center child text
             justify_content: JustifyContent::Center,
+            // vertically center child text
+            align_items: AlignItems::Center,
             ..default()
         },
+        BorderColor(Color::BLACK),
+        BorderRadius::MAX,
+        // TODO: color palette asset
+        BackgroundColor(Color::srgb_u8(76, 146, 212)),
         children![(
-            Button,
-            Node {
-                width: Val::Px(250.0),
-                height: Val::Px(65.0),
-                border: UiRect::all(Val::Px(5.0)),
-                // horizontally center child text
-                justify_content: JustifyContent::Center,
-                // vertically center child text
-                align_items: AlignItems::Center,
+            Text::new(text),
+            TextFont {
+                font: assets.button_font.clone(),
+                font_size: 33.0,
                 ..default()
             },
-            BorderColor(Color::BLACK),
-            BorderRadius::MAX,
-            // TODO: color palette asset
-            BackgroundColor(Color::srgb_u8(76, 146, 212)),
-            children![(
-                Text::new(text),
-                TextFont {
-                    font: assets.button_font.clone(),
-                    font_size: 33.0,
-                    ..default()
-                },
-                TextColor(Color::srgb(0.9, 0.9, 0.9)),
-                TextShadow::default(),
-            )]
+            TextColor(Color::srgb(0.9, 0.9, 0.9)),
+            TextShadow::default(),
         )],
     )
 }
 
-fn handle_buttons(
+fn update_button_color(
     mut interaction_query: Query<
-        (
-            &Interaction,
-            &mut BackgroundColor,
-            &mut BorderColor,
-            &Children,
-        ),
+        (&Interaction, &mut BackgroundColor, &mut BorderColor),
         (Changed<Interaction>, With<Button>),
     >,
 ) {
+    for (interaction, mut color, mut border_color) in interaction_query.iter_mut() {
+        match *interaction {
+            Interaction::Pressed => {
+                *color = Color::srgb_u8(200, 200, 200).into();
+                border_color.0 = Color::srgb_u8(200, 41, 13);
+            }
+            Interaction::Hovered => {
+                *color = Color::srgb_u8(176, 146, 112).into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                *color = Color::srgb_u8(76, 146, 212).into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
+}
+fn menu_action(
+    interaction_query: Query<(&Interaction, &MenuAction), (Changed<Interaction>, With<Button>)>,
+    mut app_exit_events: EventWriter<AppExit>,
+    mut app_state: ResMut<NextState<AppState>>,
+) {
+    for (interaction, menu_action) in &interaction_query {
+        if *interaction == Interaction::Pressed {
+            match menu_action {
+                MenuAction::StartGame => app_state.set(AppState::LoadingLevel),
+                MenuAction::Quit => {
+                    app_exit_events.write(AppExit::Success);
+                }
+            }
+        }
+    }
 }
 
 fn teardown_main_menu(mut commands: Commands, menu_items: Query<Entity, With<MenuItem>>) {
